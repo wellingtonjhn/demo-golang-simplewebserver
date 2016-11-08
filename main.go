5d532febc5ba08632c4fc2233cbc001a5a501e0d
Wellington Nascimento <wellington.jhn@gmail.com>
2016-11-08 04:20:14 -0200

    Adicionada renderização de página html estática

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"sync"
 )
 
+var count int
+var mu sync.Mutex
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, World!")
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+func static(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
 func main() {
+	http.HandleFunc("/", echoString)
+	http.HandleFunc("/count", counter)
+	http.HandleFunc("/static/", static)
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello, %q", html.EscapeString(request.URL.Path))
-	})
+	log.Fatal(http.ListenAndServe(":8081", nil))
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
